fa86a169c561a23e6f71169aeb9375941d44be1a
Ankan Nandy <ankannandy03@gmail.com>
2025-11-10 13:53:54 +0530

    small changes

--- admin-dashboard/src/components/Layout/Sidebar.jsx
+++ admin-dashboard/src/components/Layout/Sidebar.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Sidebar = () => {
   return (
     <div className="transition-duration-300 ease-in-out bg-white/80dark:bg-slate-900/80backdrop-blur-xl border-r border-slate-200/50 dark:border-slate-700/50 flex flex-col relative z-10">
-      jhhg
+        
     </div>
   )
 }
